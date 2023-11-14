5d5cacc855c4053b7618747e5b94d0e6a9fb3e69
keeser <1326658417@qq.com>
2023-11-14 20:38:27 +0800

    修复用gtest的 bug

--- test/MainTest.cpp
+++ test/MainTest.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 #include <OpenRGA/imread.h>
-// #include "gtest/gtest.h"
+#include "gtest/gtest.h"
+
+// 初始化读入图片
+TEST(OpenRGA, imread)
+{
+    auto image = RGA::imread("/home/orangepi/OpenRGA/assets/scut.jpg");
+    auto image_data = image.getMatInfo();
+    auto mat = image.src_buf_;
+    mat[1000] = '\0';
+    ASSERT_EQ(image_data.src_width, 537);
+    ASSERT_EQ(image_data.src_height, 541);
+    std::cout << "image_data: " << mat << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-
-    auto image = RGA::imread("/home/orangepi/OpenRGA/assets/scut.jpg");
-    auto image_data = image.retMatData();
-    auto mat = image.retMat();
-    mat[1000] = '\0';
-    std::cout << "src_width: " << image_data.src_width << std::endl;
-    std::cout << "src_height: " << image_data.src_height << std::endl;
-    std::cout << "image_data: " << mat << std::endl;
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
-
-// 初始化读入图片
-// TEST(OpenRGA, imread)
-// {
-//     auto image = RGA::imread("../assets/scut.jpg");
-//     auto image_data = image.retMatData();
-//     ASSERT_EQ(image_data.src_width, 537);
-//     ASSERT_EQ(image_data.src_height, 541);
-// }
-
-// testing::InitGoogleTest(&argc, argv);
-// return RUN_ALL_TESTS();
